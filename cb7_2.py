b6b734e91c63abdad7ecf8309d7207caf00ab9bb
Niels Lindberg <eighthourcream@gmail.com>
2016-10-24 13:39:05 +0200

    added bigram word bags sentiment

--- cb7_2.py
+++ cb7_2.py
@@ -3,7 +3,8 @@ from nltk.corpus import stopwords
 from nltk.corpus import movie_reviews
 from nltk.classify import NaiveBayesClassifier
 from nltk.classify.util import accuracy
-
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
 
 def bag_of_words(words):
     return dict([(word, True) for word in words])
@@ -18,7 +19,14 @@ def bag_of_non_stopwords(words, stopfile='english'):
     return bag_of_words_not_in_set(words, badwords)
 
 
-def label_feats_from_corpus(corp, feature_detector=bag_of_non_stopwords):
+def bag_of_bigrams_words(words, score_fn=BigramAssocMeasures.chi_sq, n=200):
+    bigram_finder = BigramCollocationFinder.from_words(words)
+    bigrams = bigram_finder.nbest(score_fn, n)
+    words_list = [word for word in words]
+    return bag_of_non_stopwords(words_list + bigrams)
+
+
+def label_feats_from_corpus(corp, feature_detector=bag_of_bigrams_words):
     label_feats = collections.defaultdict(list)
     # For Every Category in the Corpus
     for label in corp.categories():
@@ -40,29 +48,16 @@ def split_label_feats(lfeats, split=0.75):
     test_feats = []
     for label, feats in lfeats.items():
         cutoff = int(len(feats) * split)
-
+        print(feats)
         train_feats.extend([(feat, label) for feat in feats[:cutoff]])
         test_feats.extend([(feat, label) for feat in feats[cutoff:]])
     return train_feats, test_feats
 
 
-lfeats = label_feats_from_corpus(movie_reviews)
-print(lfeats.keys())
+lfeats = label_feats_from_corpus(movie_reviews, bag_of_words)
 
 train_feats, test_feats = split_label_feats(lfeats, split=0.75)
-print(len(train_feats))
-print(len(test_feats))
 
 nb_classifier = NaiveBayesClassifier.train(train_feats)
-print(nb_classifier.labels())
-
-negfeat = bag_of_non_stopwords(['the', 'plot', 'was', 'ludicrous'])
-posfeat = bag_of_non_stopwords(['spaceman', 'stupid', 'clever',
-                                'matt', 'damon'])
-
-print(negfeat)
-print(posfeat)
-
-print(nb_classifier.classify(negfeat))
-print(nb_classifier.classify(posfeat))
 print(accuracy(nb_classifier, test_feats))
+nb_classifier.show_most_informative_features()
